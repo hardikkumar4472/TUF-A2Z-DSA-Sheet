b2653b20c212ac9ee48049cc1ad84e229e0a404b
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2024-10-03 18:33:37 +0530

    Add files via upload

---
+++ DAY_03/CountDigits.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    int evenlyDivides(int N){
+        int count=0;
+        int temp=N;
+        while(N!=0){
+            int d=N%10;
+            N/=10;
+            if ( d>0 && temp%d==0){
+                count++;
+            }
+        }
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.evenlyDivides(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
